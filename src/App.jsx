be74b20189cf4e144cffecf29b9ee55e37f9799e
gomez-fernando <ferdgomez@hotmail.es>
2022-12-29 22:45:58 +0100

    Create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { Container, Grid, Typography } from '@mui/material';
+import NewsForm from './components/NewsForm';
 
 function App() {
 
   return (
-    <h1>news app</h1>
+    <Container>
+      <header>
+        <Typography align="center" marginY={5} component="h1" variant="h3">
+          Buscador de noticias
+        </Typography>
+      </header>
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} md={6} lg={4}>
+          <NewsForm />
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
 
