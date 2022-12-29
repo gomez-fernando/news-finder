5fb99d9012d14f376a8d2300424da932624ec977
gomez-fernando <ferdgomez@hotmail.es>
2022-12-29 23:32:33 +0100

    Create context and hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
 import { Container, Grid, Typography } from '@mui/material';
 import NewsForm from './components/NewsForm';
+import { NewsProvider } from './context/NewsProvider';
 
 function App() {
 
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component="h1" variant="h3">
-          Buscador de noticias
-        </Typography>
-      </header>
+    <NewsProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h3">
+            Buscador de noticias
+          </Typography>
+        </header>
 
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} md={6} lg={4}>
-          <NewsForm />
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <NewsForm />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </NewsProvider>
   )
 }
 
