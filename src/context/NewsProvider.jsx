2e3445f749edc85287c83c5e7f5054302923a0bf
gomez-fernando <ferdgomez@hotmail.es>
2022-12-30 11:17:39 +0100

    Add api call and news state

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,9 +1,24 @@
+import axios from "axios"
 import { useState, useEffect, createContext } from "react"
 
 const NewsContext = createContext()
 
 const NewsProvider = ({children}) => {
   const [category, setCategory] = useState('general')
+  const [news, setNews] = useState([])
+
+  useEffect(() => {
+    const callApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+      const {data} = await axios(url)
+
+      setNews(data.articles);
+    }
+
+    callApi()
+
+  }, [category])
 
   const handleChangeCategory = (e) => {
     setCategory(e.target.value)
